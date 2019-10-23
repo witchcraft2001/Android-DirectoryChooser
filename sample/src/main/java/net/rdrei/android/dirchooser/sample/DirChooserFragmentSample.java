a58cfa8dc503454a14a298aa7f99a9c7f1894b56
Dmitry Mikhaltchenkov <mikhaltchenkov@gmail.com>
2019-10-23 14:51:09 +0300

    Update for AndroidX supporting

--- sample/src/main/java/net/rdrei/android/dirchooser/sample/DirChooserFragmentSample.java
+++ sample/src/main/java/net/rdrei/android/dirchooser/sample/DirChooserFragmentSample.java
@@ -2,8 +2,8 @@ package net.rdrei.android.dirchooser.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.view.View;
 import android.widget.TextView;
 
